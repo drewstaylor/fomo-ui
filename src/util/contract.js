8712ef5b90e74d6ad5e320f9be42d71700feeecc
Drew Taylor <drew@chainofinsight.com>
2024-01-11 15:23:36 -0500

    Adds all contract functions (queries and txs)

---
+++ src/util/contract.js
@@ -0,0 +1,104 @@
+import { coin } from "@cosmjs/stargate";
+import { Client } from './client';
+import { FromAtto } from './denom';
+
+const FOMO_CONTRACT = process.env.VUE_APP_FOMO_CONTRACT;
+
+// Queries
+
+/**
+ * Retrieve the current game state
+ * @param {SigningCosmWasmClient} client? :  (Optional) instance of signing client
+ * @returns {QueryResult}
+ */
+async function Game(client = null) {
+  if (!client) client = await Client();
+  try {
+    let entrypoint = {game: {}};
+    let query = await client.wasmClient.queryClient.wasm.queryContractSmart(
+      FOMO_CONTRACT,
+      entrypoint
+    );
+    return query;
+  } catch(e) {
+    console.error(e);
+    return {};
+  }
+}
+
+// Txs
+
+/**
+ * Deposit to the game contract, increasing the timer and become 
+ * the next game leader (last depositor)
+ * @param {Number} amount : Deposit amount in ATTO
+ * @param {SigningCosmWasmClient} client? :  (Optional) instance of signing client
+ * @returns {ExecuteResult}
+ */
+async function Deposit(amount = 0, client = null) {
+  if (typeof amount !== 'number') return;
+  if (amount <= 0) return;
+  if (!client) client = await Client();
+  try {
+    let funds = [coin(String(amount), client.chainInfo.currencies[0].coinMinimalDenom)];
+    let deposit = FromAtto(amount);
+    let entrypoint = {
+      deposit: {}
+    };
+    let accounts = await client.offlineSigner.getAccounts();
+    let tx = await client.wasmClient.execute(
+      accounts[0].address,
+      FOMO_CONTRACT,
+      entrypoint,
+      client.fees,
+      "Fomo-ing in with " + deposit,
+      funds
+    );
+    return tx;
+  } catch (e) {
+    console.error(e);
+    return {
+      error: String(e)
+    };
+  }
+}
+
+/**
+ * Claim the prize funds if you won the game
+ * @param {SigningCosmWasmClient} client? :  (Optional) instance of signing client
+ * @returns {ExecuteResult}
+ */
+async function Claim(client = null) {
+  if (typeof amount !== 'number') return;
+  if (amount <= 0) return;
+  if (!client) client = await Client();
+  try {
+    let entrypoint = {
+      claim: {}
+    };
+    let accounts = await client.offlineSigner.getAccounts();
+    let tx = await client.wasmClient.execute(
+      accounts[0].address,
+      FOMO_CONTRACT,
+      entrypoint,
+      client.fees,
+      "Did I win?"
+    );
+    return tx;
+  } catch (e) {
+    console.error(e);
+    return {
+      error: String(e)
+    };
+  }
+}
+
+
+// Exports
+const Query = { Game };
+const Execute = { Deposit, Claim};
+
+export {
+  Query,
+  Execute
+}
--- src/util/denom.js
+++ src/util/denom.js
@@ -6,7 +6,7 @@ const ATTO_UNIT = 1_000_000_000_000_000_000;
  * Convert atomic-arch to arch value
  * @param {Number} value : A denomination of arch in aarch
  */
-function FromAtto(value, bigint = false) {
+function FromAtto(value, bigint = true) {
   if (bigint) return BigInt(value) / BigInt(ATTO_UNIT);
   else return value / ATTO_UNIT;
 }
@@ -14,7 +14,7 @@ function FromAtto(value, bigint = false) {
  * Convert arch to atomic-arch value
  * @param {Number} value : A denomination of arch in arch
  */
-function ToAtto(value, bigint = false) {
+function ToAtto(value, bigint = true) {
   if (bigint) return BigInt(value) * BigInt(ATTO_UNIT);
   else return value * ATTO_UNIT;
 }
